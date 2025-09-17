57fd476eaf025e1b87098df0ae5c68ed03b04db4
yj_lee <yj_lee1@atecmobility.com>
2025-09-17 16:33:00 +0900

    [fix] : 카톡 초대장 수정

--- src/components/KakaoShareButton.jsx
+++ src/components/KakaoShareButton.jsx
@@ -2,24 +2,28 @@ import React, { useEffect } from 'react';
 import kakao from '@/images/kakao.png';
 
 const KakaoShareButton = () => {
+
   useEffect(() => {
-    // Kakao 객체가 존재하고 초기화되지 않았으면 초기화
-    if (window.Kakao && !window.Kakao.isInitialized()) {
-      window.Kakao.init(import.meta.env.VITE_APP_JAVASCRIPT);
+    const { Kakao } = window;
+
+    if (!Kakao) return; // SDK가 로드되지 않았으면 종료
+
+    // Kakao 객체가 초기화되지 않았다면 초기화
+    if (!Kakao.isInitialized?.()) {
+      Kakao.init(import.meta.env.VITE_KAKAOMAP_KEY); // Vite 환경변수 사용
+      console.log('Kakao 초기화 완료:', Kakao.isInitialized());
     }
   }, []);
 
   const shareToKakao = () => {
-    if (!window.Kakao || !window.Kakao.Link) {
+    const { Kakao } = window;
+
+    if (!Kakao || !Kakao.Link) {
       alert('Kakao SDK가 아직 로드되지 않았습니다.');
       return;
     }
 
-    window.Kakao.Link.sendCustom({
-      templateId: 120646, // 실제 템플릿 ID
-    });
-  };
-
+  }
   return (
     <div className='kakao-box'>
       <img className="map-icon" src={kakao} />
