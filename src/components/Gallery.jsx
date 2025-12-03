035a2ddf0ff36dcd7fd5e4c9692fb61ba0225f16
yj_lee <yj_lee1@atecmobility.com>
2025-12-03 16:20:42 +0900

    세로 스크롤 방지 테스트

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,39 +1,55 @@
-import React from 'react'
-import '@/css/Gallery.css'
+import React, { useEffect, useRef } from 'react';
+import '@/css/Gallery.css';
 
 const Gallery = ({ images, setSelectedIndex }) => {
+  const wrapperRef = useRef(null);
 
-    // 이미지를 3개씩 잘라서 세로 column 배열로 만듦
-    const columns = [];
-    for (let i = 0; i < images.length; i += 3) {
-        columns.push(images.slice(i, i + 3));
-    }
+  // 이미지를 3개씩 잘라서 세로 column 배열로 만듦
+  const columns = [];
+  for (let i = 0; i < images.length; i += 3) {
+    columns.push(images.slice(i, i + 3));
+  }
 
-    return (
-        <div className="scroll-wrapper-horizontal" onTouchStart={(e) => e.stopPropagation()} onTouchMove={(e) => e.preventDefault()}>
-            <div className="horizontal-gallery">
-                {columns.map((group, colIdx) => (
-                    <div className="gallery-column" key={colIdx}>
-                        {group.map((src, idx) => {
-                            const globalIndex = colIdx * 3 + idx;
-                            return (
-                                <div
-                                    className="gallery-item"
-                                    key={globalIndex}
-                                    onClick={() => setSelectedIndex(globalIndex)}
-                                >
-                                    <img src={src} alt={`img-${idx}`} />
-                                    <div className="overlay">
-                                        <span>클릭해서 확대</span>
-                                    </div>
-                                </div>
-                            )
-                        })}
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
-}
+  useEffect(() => {
+    const wrapper = wrapperRef.current;
 
-export default Gallery
+    const handleTouchMove = (e) => {
+      e.stopPropagation();
+    };
+
+    // passive: false로 설정해야 preventDefault가 동작
+    wrapper.addEventListener('touchmove', handleTouchMove, { passive: false });
+
+    return () => {
+      wrapper.removeEventListener('touchmove', handleTouchMove);
+    };
+  }, []);
+
+  return (
+    <div className="scroll-wrapper-horizontal" ref={wrapperRef}>
+      <div className="horizontal-gallery">
+        {columns.map((group, colIdx) => (
+          <div className="gallery-column" key={colIdx}>
+            {group.map((src, idx) => {
+              const globalIndex = colIdx * 3 + idx;
+              return (
+                <div
+                  className="gallery-item"
+                  key={globalIndex}
+                  onClick={() => setSelectedIndex(globalIndex)}
+                >
+                  <img src={src} alt={`img-${idx}`} />
+                  <div className="overlay">
+                    <span>클릭해서 확대</span>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
